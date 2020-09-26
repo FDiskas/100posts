f58906b7deaa26c212226eecf9ac10e76dc1c4b3
Vytenis Kuciauskas <projektas@gmail.com>
2020-09-26 13:07:24 +0300

    Reorganize structure

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'zivver'`, () => {
+  it(`should have as title '100posts'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('zivver');
+    expect(app.title).toEqual('100posts');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('zivver app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('100posts app is running!');
   });
 });
